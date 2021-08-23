99818beb10cf283ee774567a68215a88b76ce2f1
Abhijeet Rajput <88309001+abhijeetrajput-coditas@users.noreply.github.com>
2021-08-23 18:41:00 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'interpolation';
+  serverId: number = 10;
+  serverName: string = "Cod;tas";
+  serverStatus: string = "offline";
+
+  designation = [{name: "Sr.1", designation:" Jr Software Associate"},
+                 {name: "Sr.2", designation:"Senior Software Associate"},
+                 {name: "Sr.3", designation:"Senior Software Associate ( Tech Lead )"}
+                
+                
+]}
+
+
